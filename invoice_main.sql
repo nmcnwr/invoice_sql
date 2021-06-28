796e495cff7e1bb096be0e56e26457a3d6f68920
Кузнецов Павел <Pavel.Kuznetsov@bercut.com>
2021-06-28 14:16:01 +0300

    subs_history

--- invoice_main.sql
+++ invoice_main.sql
@@ -1,8 +1,8 @@
 WITH SETTING AS
     (
-        select 62290320                                                as SUBS_ID
-             , DATE '2020-06-05'                                       as TIME_START
-             , TO_DATE('2021-06-25 11:40:00', 'YYYY-MM-DD HH24:MI:SS') as TIME_STOP
+        select 59563601                                                as SUBS_ID
+             , DATE '2019-06-05'                                       as TIME_START
+             , TO_DATE('2020-06-25 11:40:00', 'YYYY-MM-DD HH24:MI:SS') as TIME_STOP
              , 1                                                       as VIEW_DATES_INVOICE
              , 1                                                       as VIEW_PAYMENT
              , '(\wаланс:?\s+.\d*.\d\d\s+руб\.)'                       as REGEXP_01
@@ -235,18 +235,53 @@ WITH SETTING AS
           AND T1.CRE_DATE > (select TIME_START from SETTING)
           AND T1.CRE_DATE < (select TIME_STOP from SETTING)
     )
+   , X109 as
+    (
+        select '75_SUBS_HISTORY'                                                                               TNAME
+             , CLNT_ID ||
+               decode(DEF_PAY_PACK_CLNT, null, null, DEF_PAY_PACK_CLNT, ' Pay by: ' || DEF_PAY_PACK_CLNT)      CLNT_ID
+             --, DD.ST_ID || ' ' || D4.SUBS_TYPE                                                            ST_ID
+             , DD.STAT_ID || ' ' || D3.STATUS                                                                  STAT_ID
+             , DD.TRPL_ID || ' ' || D6.TRPL_NAME                                                               TRPL_SERV
+             , STIME                                                                                           ORDER_TIME
+             , ETIME                                                                                           END_DATE
+             , DD.CRE_USER_ID || ' ' || D2.LOGIN                                                               CRE_USER_ID
+             , DD.CRE_DATE
+             , DECODE(DD.CSC_ID, null, null, BLOCK_QUOTA, 'CSC_ID=' || DD.CSC_ID || ' ' || D5.CSC_NAME || ';') COMMENT_1
+             , DECODE(DD.TAL_ID, null, null, DD.TAL_ID, 'TAL_ID=' || DD.TAL_ID || ' ' || D1.TAL_NAME || ';')
+            || DECODE(BLOCK_QUOTA, null, null, BLOCK_QUOTA, 'BLOCK_QUOTA=' || BLOCK_QUOTA || ';')
+            || DECODE(CHANGE_TRPL, null, null, 0, 'CHANGE_TRPL=0(не учитывать смену ТП);', CHANGE_TRPL,
+                      'CHANGE_TRPL=' || CHANGE_TRPL || ';')
+            || DECODE(PHONE_ID, null, null, PHONE_ID, 'PHONE.PHONE_ID=' || PHONE_ID || ';')
+            || DECODE(SORD_ID, null, null, SORD_ID, 'SERV_ORDER.SORD_ID=' || SORD_ID || ';')                   COMMENT_2
+
+
+        from SUBS_HISTORY DD
+                 LEFT JOIN TAR_ALLOW_LEVEL D1 on D1.TAL_ID = DD.TAL_ID
+                 INNER JOIN INV_USER D2 ON D2.USER_ID = DD.CRE_USER_ID
+                 LEFT JOIN STATUS D3 on D3.STAT_ID = DD.STAT_ID
+                 INNER JOIN SUBS_TYPE D4 on D4.ST_ID = DD.ST_ID
+                 LEFT JOIN COUSE_STATUS_CHANGE D5 ON D5.CSC_ID = DD.CSC_ID
+                 LEFT JOIN TARIFF_PLAN D6 ON D6.TRPL_ID = DD.TRPL_ID
+        WHERE 3 = 3
+          AND DD.SUBS_ID = (SELECT SUBS_ID FROM SETTING)
+          AND DD.CRE_DATE > (select TIME_START from SETTING)
+          AND DD.CRE_DATE < (select TIME_STOP from SETTING)
+    )
 
 
 SELECT V_INVOICE
-     , COALESCE(TT.TNAME, X101.TNAME, X102.TNAME, X104.TNAME, X105.TNAME, X106.TNAME, X107.TNAME, X108.TNAME) TNAME
+     , X109.CLNT_ID
+     , COALESCE(TT.TNAME, X101.TNAME, X102.TNAME, X104.TNAME, X105.TNAME, X106.TNAME, X107.TNAME, X108.TNAME,
+                X109.TNAME)                                                                                TNAME
      , COALESCE(TT.CRE_USER_ID, X102.CRE_USER_ID, X104.CRE_USER_ID, X106.CRE_USER_ID, X105.CRE_USER_ID,
-                X107.CRE_USER_ID, X108.CRE_USER_ID)                                                           CRE_USER_ID
+                X107.CRE_USER_ID, X108.CRE_USER_ID, X109.CRE_USER_ID)                                      CRE_USER_ID
      , COALESCE(DD.D_A_T_E, TT.CRE_DATE, X102.CRE_DATE, X104.CRE_DATE, X106.CRE_DATE, X101.CRE_DATE, X105.CRE_DATE,
-                X107.CRE_DATE, X108.CRE_DATE)                                                                 CRE_DATE
-     , COALESCE(X106.ORDER_TIME, X105.ORDER_TIME, X107.ORDER_TIME, X108.ORDER_TIME, null)                     ORDER_TIME
-     , COALESCE(TT.END_DATE, X102.END_DATE, X104.END_DATE, X105.END_DATE, X108.END_DATE)                      END_DATE
-     , COALESCE(X102.SUMM_$, X101.SUMM_$)                                                                     PAY_$
-     , DECODE(X104.CHARGE_VALUE, 0, null, X104.CHARGE_VALUE)                                                  CHARGE_$
+                X107.CRE_DATE, X108.CRE_DATE, X109.CRE_DATE)                                               CRE_DATE
+     , COALESCE(X106.ORDER_TIME, X105.ORDER_TIME, X107.ORDER_TIME, X108.ORDER_TIME, X109.ORDER_TIME, null) ORDER_TIME
+     , COALESCE(TT.END_DATE, X102.END_DATE, X104.END_DATE, X105.END_DATE, X108.END_DATE, X109.END_DATE)    END_DATE
+     , COALESCE(X102.SUMM_$, X101.SUMM_$)                                                                  PAY_$
+     , DECODE(X104.CHARGE_VALUE, 0, null, X104.CHARGE_VALUE)                                               CHARGE_$
      , REGEXP_REPLACE(
         REGEXP_REPLACE(
                 coalesce(
@@ -254,11 +289,13 @@ SELECT V_INVOICE
                         REGEXP_SUBSTR(tt.COMMENT_1, (select REGEXP_02 from SETTING)),
                         REGEXP_SUBSTR(tt.COMMENT_2, (select REGEXP_03 from SETTING)))
             , ':|\s|\.$|[[:alpha:]]', '')
-    , ',', '.')                                                                                               BALANCE_$
-     , COALESCE(TT.TRPL_SERV, X104.TRPL_SERV, X105.TRPL_SERV, X106.TRPL_SERV, X107.TRPL_SERV, X108.TRPL_SERV) TRPL_SERV
+    , ',', '.')                                                                                            BALANCE_$
+     , COALESCE(TT.TRPL_SERV, X104.TRPL_SERV, X105.TRPL_SERV, X106.TRPL_SERV, X107.TRPL_SERV, X108.TRPL_SERV,
+                X109.TRPL_SERV)                                                                            TRPL_SERV
      , COALESCE(TT.COMMENT_1, X102.COMMENT_1, X104.COMMENT_1, X101.COMMENT_1,
-                X105.COMMENT_1, X107.COMMENT_1, X108.COMMENT_1)                                               COMMENTS_1
-     , COALESCE(TT.COMMENT_2, X104.COMMENT_2, X101.COMMENT_2, X105.COMMENT_2, X107.COMMENT_2, X108.COMMENT_2) COMMENTS_2
+                X105.COMMENT_1, X107.COMMENT_1, X108.COMMENT_1, X109.COMMENT_1)                            COMMENTS_1
+     , COALESCE(TT.COMMENT_2, X104.COMMENT_2, X101.COMMENT_2, X105.COMMENT_2, X107.COMMENT_2, X108.COMMENT_2,
+                X109.COMMENT_2)                                                                            COMMENTS_2
 
 FROM TT
          FULL OUTER JOIN DD ON TT.TNAME = DD.TNAME
@@ -269,10 +306,10 @@ FROM TT
          FULL OUTER JOIN X106 ON TT.TNAME = X106.TNAME
          FULL OUTER JOIN X107 ON TT.TNAME = X107.TNAME
          FULL OUTER JOIN X108 ON TT.TNAME = X108.TNAME
+         FULL OUTER JOIN X109 ON TT.TNAME = X109.TNAME
 
-ORDER BY
-         COALESCE(DD.D_A_T_E, TT.CRE_DATE, X101.CRE_DATE, X102.CRE_DATE, X104.CRE_DATE
-                  , X105.CRE_DATE, X106.CRE_DATE, X107.CRE_DATE, X108.CRE_DATE)
+ORDER BY COALESCE(DD.D_A_T_E, TT.CRE_DATE, X101.CRE_DATE, X102.CRE_DATE, X104.CRE_DATE
+    , X105.CRE_DATE, X106.CRE_DATE, X107.CRE_DATE, X108.CRE_DATE, X109.CRE_DATE)
        , TNAME
        , COALESCE(X102.SUMM_$, X104.CHARGE_VALUE) desc
 ;
